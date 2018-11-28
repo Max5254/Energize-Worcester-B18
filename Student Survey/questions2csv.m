5396e7d74a09e83e3fea2e8be585f56111f9d1f3
Max Westwater <maxwestwater@gmail.com>
2018-11-28 12:56:46 +0000

    Add ability to do Q_a,Q_b, etc to csv exporter

--- Student Survey/questions2csv.m
+++ Student Survey/questions2csv.m
@@ -3,7 +3,17 @@ function [cellOut] = questions2csv(dataIn,keyData,questions,fileName)
 numQuestions = length(questions);
 % cellOut = cell(1,3);
 for i = 1:numQuestions
-    currQ = sprintf('Q%d',questions{i});
+    q = questions{i};
+    if(rem(q,1)==0)
+        %% normal questions w/o sub part
+        currQ = sprintf('Q%d',q);
+    else
+        %% questions with part a/b/etc....
+        %  10.1 represents 10_a
+        dec = rem(q,1); % find decimal part of number
+        int = q-dec;    % find int part of number
+        currQ = sprintf('Q%d_%c',int,char(dec+97)); % 97 is offset in ascii to a
+    end
     d = getQuestionData(dataIn,keyData,currQ);
     qCell = cell(size(d.labels,1)+1,3);
     qCell(1,1:2) = {currQ, d.title};
